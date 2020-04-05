c0c8da573ce6ee7e8412494d5b95313911bd1df4
tushar <garg.tus@gmail.com>
2020-04-05 13:19:02 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Tushar Garg on 05/04/20.
+//  Copyright © 2020 Tushar Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
